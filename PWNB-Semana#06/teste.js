e48ac62c903b67afbcbd62d5725847f2079e7130
Vinicius Santos Lima <viniciusslsilva2@gmail.com>
2023-09-22 17:18:38 -0300

    Commits referentes a Atv. 6 CSS & Introdução a JS.

---
+++ PWNB-Semana#06/teste.js
@@ -0,0 +1,22 @@
+function chamarJS(){
+    codCliente = "1234"
+
+    if(codCliente == "1234"){
+        alert("Bem vindo!")
+    } else {
+        alert("Não conseguimos identificar seu cadastro, por favor, retorne a area de cadastros!")
+    }
+}
+
+// Crie uma função para colher e armazenar dados através do prompt
+function colheDados(){
+    var usuario;
+    var senha;
+
+}
+
+//Crie uma função só para mostrar os dados colhidos pela função anterior
+function mostraDados(){
+
+    
+}

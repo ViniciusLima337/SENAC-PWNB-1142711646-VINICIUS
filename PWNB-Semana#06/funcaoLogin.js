41f81632cae82ca4659f5ae6221a8c0f303b4b48
Vinicius Santos Lima <viniciusslsilva2@gmail.com>
2023-10-08 19:52:41 -0300

    Commit de alinhamento de projeto

--- PWNB-Semana#06/funcaoLogin.js
+++ PWNB-Semana#06/funcaoLogin.js
@@ -1,11 +1,11 @@
 function iniciarLogin(){
-
+    
     let usuario = widow.prompt('Insira nome de Usuario... ')
     let senha = widow.prompt('Agora insira sua Senha...')
 
     if(usuario == admin && senha == 1234){
-        widow.alert(`Olá, ${usuario}! Bem Vindo de volta!`)
+        window.alert(`Olá, ${usuario}! Bem Vindo de volta!`)
     }else{
-        widow.alert(`Usuario não cadastrado. Prossiga para tela de Cadastros!`)
+        window.alert(`Usuario não cadastrado. Prossiga para tela de Cadastros!`)
     }
 }
